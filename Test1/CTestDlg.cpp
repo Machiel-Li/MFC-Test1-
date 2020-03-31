5b67ab3f42c3d18105236c75da8a922ebc04409c
乔峰 <CCCCCWW@outlook.com>
2020-03-31 12:20:19 +0800

    添加项目文件。

---
+++ Test1/CTestDlg.cpp
@@ -0,0 +1,46 @@
+﻿// CTestDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "Test1.h"
+#include "CTestDlg.h"
+#include "afxdialogex.h"
+
+
+// CTestDlg 对话框
+
+IMPLEMENT_DYNAMIC(CTestDlg, CDialogEx)
+
+CTestDlg::CTestDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG_TEST, pParent)
+{
+
+}
+
+CTestDlg::~CTestDlg()
+{
+}
+
+void CTestDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CTestDlg, CDialogEx)
+	ON_EN_CHANGE(IDC_EDIT2, &CTestDlg::OnEnChangeEdit2)
+END_MESSAGE_MAP()
+
+
+// CTestDlg 消息处理程序
+
+
+void CTestDlg::OnEnChangeEdit2()
+{
+	// TODO:  如果该控件是 RICHEDIT 控件，它将不
+	// 发送此通知，除非重写 CDialogEx::OnInitDialog()
+	// 函数并调用 CRichEditCtrl().SetEventMask()，
+	// 同时将 ENM_CHANGE 标志“或”运算到掩码中。
+
+	// TODO:  在此添加控件通知处理程序代码
+}
---
+++ Test1/CTestDlg.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+
+// CTestDlg 对话框
+
+class CTestDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CTestDlg)
+
+public:
+	CTestDlg(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CTestDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_TEST };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit2();
+};
